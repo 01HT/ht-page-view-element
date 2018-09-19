02da32bb6b5c930751a7c00ea6ace2e405ee7e36
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2018-09-19 12:31:11 +0300

    changes in syntax

--- ht-page-view-element.js
+++ ht-page-view-element.js
@@ -1,14 +1,12 @@
-import { LitElement } from '@polymer/lit-element';
+import { LitElement } from "@polymer/lit-element";
 
 export class HTPageViewElement extends LitElement {
   // Only render this page if it's actually visible.
-  _shouldRender(props, changedProps, old) {
-    return props.active;
+  shouldUpdate(changedProps) {
+    return this.active;
   }
 
   static get properties() {
-    return {
-      active: Boolean
-    }
+    return { active: { type: Boolean } };
   }
 }
